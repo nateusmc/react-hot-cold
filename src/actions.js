139fc582ee6738ffb269ce1687436a4ec6e86b0d
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-13 14:31:24 -0600

    added actions

---
+++ src/actions.js
@@ -0,0 +1,29 @@
+/*
+Actions Needed:
+* New Game
+* Submit Guesses
+* Info Modal Button
+* Feedback
+*/
+
+export const NEW_GAME = 'NEW_GAME'
+export const newGame = () => {
+  type: NEW_GAME
+}
+
+export const SUBMIT_GUESSES = 'SUBMIT_GUESSES'
+export const submitGuesses = (guess) => {
+  type: SUBMIT_GUESSES,
+  guess
+}
+
+export const INFO_MODAL = 'INFO_MODAL'
+export const infoModal = () => {
+  type: INFO_MODAL
+}
+
+export const UPDATE_FEEDBACK = 'UPDATE_FEEDBACK'
+export const updateFeedback = (guess) => {
+  type: UPDATE_FEEDBACK,
+  guess
+}
