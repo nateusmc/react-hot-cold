51a242beecd914bceb29a08a7f046c001bf903aa
Andy Gaines <andyagaines@gmail.com>
2017-11-14 11:22:04 -0600

    rewired store.js index.js and completed guess-list.js

--- src/store.js
+++ src/store.js
@@ -8,15 +8,16 @@ const initialState = {
 	correctAnswer: Math.floor(Math.random() * 100) + 1,
 	modalOpen: false
 };
-
 let store = createStore(reducer, initialState);
+export default store;
 
 store.dispatch(submitGuesses(32));
 store.dispatch(submitGuesses(81));
-console.log(store.getState());
-store.dispatch(infoModal());
-console.log(store.getState());
-store.dispatch(updateFeedback(32));
-console.log(store.getState());
-store.dispatch(newGame());
-console.log(store.getState());
+store.dispatch(submitGuesses(99));
+// console.log(store.getState());
+// store.dispatch(infoModal());
+// console.log(store.getState());
+// store.dispatch(updateFeedback(32));
+// console.log(store.getState());
+// store.dispatch(newGame());
+// console.log(store.getState());
